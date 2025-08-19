abbec564eb1a46e6d0ec1b5b06be34681f3cc952
Study <studyneet9818@gmail.com>
2025-08-19 12:02:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 👈 REQUIRED for static export
-
   images: {
     loader: 'default',
     unoptimized: true, // 👈 Required if you're using <Image />
-  },
-
-  experimental: {
-    // Useful for Docker & restricted environments
-    outputFileTracingExcludes: {
-      '*': [
-        './node_modules/@swc/core-linux-x64-gnu',
-        './node_modules/@swc/core-linux-x64-musl',
-        './node_modules/esbuild-linux-64',
-      ],
-    },
   },
 };
 
