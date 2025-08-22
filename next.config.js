203f28be9d867178d31b74a33c987beccd5160af
Study <studyneet9818@gmail.com>
2025-08-22 17:57:35 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '',
   images: {
     loader: 'default',
     unoptimized: true, // 👈 Required if you're using <Image />
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+      '*.cloudworkstations.dev',
+    ],
   },
 };
 
