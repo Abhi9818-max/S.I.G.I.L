f9f8f7fd022929b94c306d79046ddda172bd2fb2
Study <studyneet9818@gmail.com>
2025-08-27 13:58:11 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     loader: 'default',
     unoptimized: true, // Keep this if you want unoptimized images
   },
-  allowedDevOrigins: [
-    '*.cloudworkstations.dev',
-  ],
 };
 
 module.exports = nextConfig;
