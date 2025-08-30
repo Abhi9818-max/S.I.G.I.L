285a5f20f5ab286859a9aac705b9121e8e66f705
Study <studyneet9818@gmail.com>
2025-08-30 17:46:43 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/app/alliances/[allianceId]/page.tsx
+++ src/app/alliances/[allianceId]/page.tsx
@@ -2,7 +2,7 @@ import ClientAlliancePage from './ClientAlliancePage';
 
 type Params = { allianceId: string };
 
-export default async function AlliancePage({ params }: { params: Params }) {
-  const { allianceId } = params;
+export default async function AlliancePage({ params }: { params: Promise<Params> }) {
+  const { allianceId } = await params;
   return <ClientAlliancePage allianceId={allianceId} />;
 }
