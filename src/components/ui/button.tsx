99ada09080acc48faf259f7ed002f51c647e3ed3
Study <studyneet9818@gmail.com>
2025-09-05 15:12:54 +0000

    see fucker in 1st video when i dont click on like button its in simpe wh

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-all duration-200 ease-in-out hover:-translate-y-px hover:shadow-lg focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 active:translate-y-0 active:shadow-inner active:scale-95",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-all duration-200 ease-in-out hover:-translate-y-px hover:shadow-lg focus-visible:outline-none focus-visible:ring-0 disabled:pointer-events-none disabled:opacity-50 active:translate-y-0 active:shadow-inner active:scale-95",
   {
     variants: {
       variant: {
