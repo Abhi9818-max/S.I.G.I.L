16f4a201d184ac97dcd7717b61993e5fc1bfba6f
Study <studyneet9818@gmail.com>
2025-09-05 14:29:46 +0000

    i updated more avatar from avatar42.jpeg to avatar53.jpeg for girls sect

--- src/components/settings/AvatarSelectionDialog.tsx
+++ src/components/settings/AvatarSelectionDialog.tsx
@@ -16,7 +16,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 // Define avatar number arrays directly here for categorization
 const MALE_AVATAR_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32];
-const FEMALE_AVATAR_NUMBERS = [13, 15, 16, 17, 33, 34, 35, 36, 37, 38, 39, 40, 41];
+const FEMALE_AVATAR_NUMBERS = [13, 15, 16, 17, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53];
 
 const MALE_AVATARS = MALE_AVATAR_NUMBERS.map(n => `/avatars/avatar${n}.jpeg`);
 const FEMALE_AVATARS = FEMALE_AVATAR_NUMBERS.map(n => `/avatars/avatar${n}.jpeg`);
