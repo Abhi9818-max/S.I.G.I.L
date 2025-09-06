e68daaa84c366d039a822b2448a386dd26448d53
Study <studyneet9818@gmail.com>
2025-09-06 08:12:07 +0000

    in phone version , increase the size by 13% . dont change it in desktop

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -131,7 +131,7 @@ export default function NotesPage() {
           </div>
         )}
         
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {filteredNotes.map((note, index) => (
             <div
               key={note.id}
